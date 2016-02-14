6c7dded3925dfc67e233d4512c2d99fcc6225645
Evandro Salvador Figueirôa <esfblip@hotmail.com>
2016-02-13 23:51:56 -0200

    Animation Running

--- Lord10/Forms/MainForm.xaml.cs
+++ Lord10/Forms/MainForm.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Windows;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -51,9 +53,15 @@ namespace Lord10.Forms
 
             spinButton.Begin();
 
-            // Chame StoryButton FLAG
-            Storyboard sb = ( sysutils.FindVisualChild<Button>(MainHub, "cButtonLAG") as Button).Resources["StoryboardOnFlag"] as Storyboard;
+            Button sb = (sysutils.FindVisualChild<Button>(MainHub, "cButtonLAG") as Button); // Referencia Button OK
+            Display_Animation_Button(sb);
+
+            // sb.Background =(Brush)Resources["BrushPadraoVerdeAtivo"]; // Carrega Resource definido em Styles não esta funbcionando Typecasting
+
+            
             // sb.Begin();
+
+
 
 
             // Chama Diag
@@ -90,6 +98,46 @@ namespace Lord10.Forms
         {
             view.log += msg;
         }
+        private void Display_Animation_Button( Button rbutt )
+        {
+            /// Criação de Gradiente manual
+            LinearGradientBrush BrushPadraoVerdeAtivo = new LinearGradientBrush();
+            BrushPadraoVerdeAtivo.StartPoint = new Point(0.5, 0);
+            BrushPadraoVerdeAtivo.EndPoint = new Point(0.5, 1);
+
+            CompositeTransform trans = new CompositeTransform();
+            trans.CenterY = 0.5;
+            trans.CenterX = 0.5;
+            trans.Rotation = 90;
+
+            BrushPadraoVerdeAtivo.RelativeTransform = trans;
+                    // Create and add Gradient stops
+                       GradientStop stop1 = new GradientStop();
+                       stop1.Color = Color.FromArgb(0x72, 00, 00, 00);
+                       stop1.Offset = 0.469;
+                       BrushPadraoVerdeAtivo.GradientStops.Add(stop1);
+                       GradientStop stop2 = new GradientStop();
+                       stop2.Color = Color.FromArgb(0x33, 0x12, 0xB0, 0x0A);
+                       stop2.Offset = 0.879;
+                       BrushPadraoVerdeAtivo.GradientStops.Add(stop2);
+                       GradientStop stop3 = new GradientStop();
+                       stop3.Color = Color.FromArgb(0x5E, 0x05, 0x35, 0x03);
+                       BrushPadraoVerdeAtivo.GradientStops.Add(stop3);
+            rbutt.Background = BrushPadraoVerdeAtivo;
+
+            Storyboard sb = (sysutils.FindVisualChild<Button>(MainHub, "cButtonLAG") as Button).Resources["StoryboardOnFlag"] as Storyboard;
+
+            sb.Begin();
+
+            /// Inicializa Storyboard
+            /// 
+
+
+
+
+            
+
+        }
     }
-  
+     
 }
